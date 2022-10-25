50e6d1a0bf679188fefb7ad76b2a4e9ddbb07c1f
jonamelibia <jon.amelibia@alumni.mondragon.edu>
2022-10-25 21:10:24 +0200

    v1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyimg_process',         # How you named your package folder (MyLib)
   packages = ['pyimg_process'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple package for image processing',   # Give a short description about your library
   author = 'Mikel Agirrebeitia & Jon Amelibia',                   # Type in your name
