b289b5361bcda0ea0208c72a30575f1e14fd0e4b
Jon <64251021+jonamelibia@users.noreply.github.com>
2022-10-25 19:17:20 +0200

    Update Methods.py

--- src/pyimg_process/Methods.py
+++ src/pyimg_process/Methods.py
@@ -2,11 +2,41 @@ from Read import Read
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import os
+    
+class Methods():
 
-class Methods(Read):
+    def __init__(self) -> None:
+        pass
 
-    def __init__(self):
-        super().__init__()
+    def read_image(self, path, filename):
+        self.filename = filename
+        self.path = path
+        file_path = os.path.join(path, filename)
+        self.img = cv2.imread(file_path)
+
+        if self.img is None:
+            raise Exception("Sorry, image not found")
+
+        else:
+            return self.img
+
+    def write_image(self, filename, img, same_path=True, **kwargs):
+        if same_path:
+            file_path = os.path.join(self.path, filename)
+        else:
+            file_path = os.path.join(kwargs["path"], filename)
+
+        cv2.imwrite(file_path, img)
+        print(f"Image written in {file_path}")
+
+    def show_image(self, img):
+        plt.figure()
+        if img.shape[-1] == 3:
+            plt.imshow(img[:,:,::-1])
+        else:
+            plt.imshow(img, cmap="gray")
+        plt.show()
     
     def resize(self, img, output_size):
         new_x = output_size[0]
