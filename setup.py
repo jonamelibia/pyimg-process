69a01e607dbcc0e11b04d5a78e54c043e79f98c7
MikelAgirrebeitia <mikel.agirrebeitia@alumni.mondragon.edu>
2022-10-26 11:27:29 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyimg_process',         # How you named your package folder (MyLib)
   packages = ['pyimg_process'],   # Chose the same as "name"
-  version = '1.2',      # Start with a small number and increase it with every change you make
+  version = '1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple package for image processing',   # Give a short description about your library
   author = 'Mikel Agirrebeitia & Jon Amelibia',                   # Type in your name
   author_email = 'jon.amelibia@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/jonamelibia/pyimg-process',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.3.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
