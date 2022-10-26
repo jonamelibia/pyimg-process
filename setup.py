212c96fc1a1d23ffb89ebc1ae356e7a1a65ad218
MikelAgirrebeitia <64250899+MikelAgirrebeitia@users.noreply.github.com>
2022-10-26 15:27:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-import pypandoc
 from distutils.core import setup
-#from pathlib import Path
-#this_directory = Path(__file__).parent
-#long_description = (this_directory / "README.md").read_text()
-long_description = pypandoc.convert_file('README.md', 'rst')
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
   name = 'pyimg_process',         # How you named your package folder (MyLib)
   packages = ['pyimg_process'],   # Chose the same as "name"
-  version = '1.3.2',      # Start with a small number and increase it with every change you make
+  version = '1.3.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple package for image processing',   # Give a short description about your library
   author = 'Mikel Agirrebeitia & Jon Amelibia',                   # Type in your name
   author_email = 'jon.amelibia@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/jonamelibia/pyimg-process',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.3.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.3.3.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   long_description_content_type="text/markdown",
   long_description=long_description,
