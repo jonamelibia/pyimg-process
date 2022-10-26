333abe675ffb47b3473279e5f12a7fc332ef374b
MikelAgirrebeitia <mikel.agirrebeitia@alumni.mondragon.edu>
2022-10-26 15:57:46 +0200

    1234234 actualization

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+esfrom distutils.core import setup
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
@@ -6,13 +6,13 @@ long_description = (this_directory / "README.md").read_text()
 setup(
   name = 'pyimg_process',         # How you named your package folder (MyLib)
   packages = ['pyimg_process'],   # Chose the same as "name"
-  version = '1.3.3',      # Start with a small number and increase it with every change you make
+  version = '1.3.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple package for image processing',   # Give a short description about your library
   author = 'Mikel Agirrebeitia & Jon Amelibia',                   # Type in your name
   author_email = 'jon.amelibia@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/jonamelibia/pyimg-process',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.3.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.3.4.tar.gz',    # I explain this later on
   keywords = ['Image proceessing', 'Easy to use', 'Simple'],   # Keywords that define your package best
   long_description=long_description,
   long_description_content_type="text/markdown",
