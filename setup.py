c00ff4bee67e95504b77c09981efe8f959c090f0
MikelAgirrebeitia <64250899+MikelAgirrebeitia@users.noreply.github.com>
2022-10-26 15:04:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
-!pip3 install pypandoc
-!pip3 install pathlib
 from distutils.core import setup
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = open('README.md').read()
 
 setup(
   name = 'pyimg_process',         # How you named your package folder (MyLib)
