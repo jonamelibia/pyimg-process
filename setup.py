67557100bdb6fa238454b9cfa8b45ebf1a10e4bb
MikelAgirrebeitia <64250899+MikelAgirrebeitia@users.noreply.github.com>
2022-10-26 12:02:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
   version = '1.3.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple package for image processing',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type='text/markdown',
   author = 'Mikel Agirrebeitia & Jon Amelibia',                   # Type in your name
   author_email = 'jon.amelibia@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/jonamelibia/pyimg-process',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/jonamelibia/pyimg-process/archive/refs/tags/1.3.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   install_requires=[            # I get to this in a second
           'numpy',
           'matplotlib',
